986c1fb6d9215cea34882337adc5fdafebabdc8e
danilo <danilosindric@gmail.com>
2025-02-05 19:35:08 +0100

    Update: add real profile picture to user

--- assets/vue/src/components/ProfileDropdown.vue
+++ assets/vue/src/components/ProfileDropdown.vue
@@ -2,9 +2,9 @@
     <DropdownMenu>
         <DropdownMenuTrigger as-child>
             <Button variant="ghost" size="sm">
-                <img class="size-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt="" v-if="authStore.authUser">
+                <img v-if="authStore.authUser"
+                    :src="`https://api.dicebear.com/9.x/initials/svg/seed=${authStore.authUser.first_name}-${authStore.authUser.last_name}`"
+                    alt="avatar" class="size-8 rounded-full" />
                 <UserIcon v-else />
             </Button>
         </DropdownMenuTrigger>
