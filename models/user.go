a13b4c036c855b1648d98a2a97584be39448290e
danilo <danilosindric@gmail.com>
2025-02-06 17:47:21 +0100

    Update: add role attribute to user

--- models/user.go
+++ models/user.go
@@ -12,7 +12,8 @@ type User struct {
 	FirstName string    `json:"first_name" validate:"required"`
 	LastName  string    `json:"last_name" validate:"required"`
 	Email     string    `gorm:"uniqueIndex" json:"email" validate:"required,email"`
-	Password  []byte	`validate:"required,min=8" json:"-"`
+	Password  []byte    `validate:"required,min=8" json:"-"`
+	Role      string    `json:"role" gorm:"default:candidate"`
 
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
