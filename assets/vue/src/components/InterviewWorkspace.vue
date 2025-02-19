8313d1459fc7435a84c006dff30a9272d3b71a3d
danilo <danilosindric@gmail.com>
2025-02-19 19:08:46 +0100

    Update: load problem from an api

--- assets/vue/src/components/InterviewWorkspace.vue
+++ assets/vue/src/components/InterviewWorkspace.vue
@@ -1,12 +1,11 @@
 <template>
-    <ResizablePanelGroup direction="vertical" class="min-h-[calc-100vh-4rem-1px]">
+    <AppLoading v-if="isLoading" />
+    <ResizablePanelGroup direction="vertical" class="min-h-[calc-100vh-4rem-1px]" v-else>
         <ResizablePanel>
             <ScrollArea class="h-full">
                 <div class="p-6">
                     <div class="max-w-4xl mx-auto space-y-6">
-                        <div
-                            class="flex flex-col sm:flex-row sm:items-center justify-between gap-4"
-                        >
+                        <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
                             <div class="space-y-1">
                                 <div class="flex items-center gap-2">
                                     <h2 class="text-2xl font-semibold tracking-tight">
@@ -18,21 +17,6 @@
                                 </div>
                             </div>
                             <div class="flex items-center gap-3">
-                                <Select v-model="selectedQuestion.titleSlug">
-                                    <SelectTrigger class="w-[180px]">
-                                        <SelectValue placeholder="Select question" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem
-                                            v-for="q in questions"
-                                            :key="q.titleSlug"
-                                            :value="q.titleSlug"
-                                        >
-                                            {{ q.title }}
-                                        </SelectItem>
-                                    </SelectContent>
-                                </Select>
-
                                 <Select v-model="language">
                                     <SelectTrigger class="w-[150px]">
                                         <SelectValue>
@@ -40,11 +24,7 @@
                                         </SelectValue>
                                     </SelectTrigger>
                                     <SelectContent>
-                                        <SelectItem
-                                            v-for="lang in supportedLanguages"
-                                            :key="lang[0]"
-                                            :value="lang[0]"
-                                        >
+                                        <SelectItem v-for="lang in supportedLanguages" :key="lang[0]" :value="lang[0]">
                                             <div class="flex items-center gap-2">
                                                 {{ lang[1] }}
                                             </div>
@@ -64,13 +44,8 @@
 
         <ResizablePanel :default-size="60" :max-size="100">
             <div class="h-full relative">
-                <VueMonacoEditor
-                    height="100%"
-                    :default-language="language"
-                    :language="language"
-                    theme="vs-dark"
-                    v-model:value="code"
-                    :options="{
+                <VueMonacoEditor height="100%" :default-language="language" :language="language" theme="vs-dark"
+                    v-model:value="code" :options="{
                         minimap: { enabled: false },
                         fontSize: 18,
                         lineNumbers: 'on',
@@ -79,8 +54,7 @@
                         padding: { top: 16, bottom: 16 },
                         wordWrap: 'on',
                         wrappingIndent: 'indent',
-                    }"
-                >
+                    }">
                 </VueMonacoEditor>
             </div>
         </ResizablePanel>
@@ -100,16 +74,10 @@ import SelectItem from './ui/select/SelectItem.vue';
 import ScrollBar from './ui/scroll-area/ScrollBar.vue';
 import ScrollArea from './ui/scroll-area/ScrollArea.vue';
 import { VueMonacoEditor } from '@guolao/vue-monaco-editor';
+import axios from 'axios';
+import { toast } from 'vue3-toastify';
+import AppLoading from './AppLoading.vue';
 
-// Placeholder for dynamic data fetching from an API
-const questions = [
-    {
-        titleSlug: 'two-sum',
-        title: 'Two Sum',
-        questionHTML:
-            '<p>Given an array of integers <code>nums</code>&nbsp;and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to <code>target</code></em>.</p>\n\n<p>You may assume that each input would have <strong><em>exactly</em> one solution</strong>, and you may not use the <em>same</em> element twice.</p>\n\n<p>You can return the answer in any order.</p>\n\n<p>&nbsp;</p>\n<p><strong class="example">Example 1:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [2,7,11,15], target = 9\n<strong>Output:</strong> [0,1]\n<strong>Explanation:</strong> Because nums[0] + nums[1] == 9, we return [0, 1].\n</pre>\n\n<p><strong class="example">Example 2:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [3,2,4], target = 6\n<strong>Output:</strong> [1,2]\n</pre>\n\n<p><strong class="example">Example 3:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [3,3], target = 6\n<strong>Output:</strong> [0,1]\n</pre>\n\n<p>&nbsp;</p>\n<p><strong>Constraints:</strong></p>\n\n<ul>\n\t<li><code>2 &lt;= nums.length &lt;= 10<sup>4</sup></code></li>\n\t<li><code>-10<sup>9</sup> &lt;= nums[i] &lt;= 10<sup>9</sup></code></li>\n\t<li><code>-10<sup>9</sup> &lt;= target &lt;= 10<sup>9</sup></code></li>\n\t<li><strong>Only one valid answer exists.</strong></li>\n</ul>\n\n<p>&nbsp;</p>\n<strong>Follow-up:&nbsp;</strong>Can you come up with an algorithm that is less than <code>O(n<sup>2</sup>)</code><font face="monospace">&nbsp;</font>time complexity?',
-    },
-];
 
 const supportedLanguages = [
     ['go', 'Go'],
@@ -118,7 +86,18 @@ const supportedLanguages = [
     ['javascript', 'Javascript'],
 ];
 
-const selectedQuestion = ref(questions[0]);
+const isLoading = ref(true);
+const selectedQuestion = ref({});
 const language = ref(supportedLanguages[0][0]);
 const code = ref('');
+
+axios.get(import.meta.env.VITE_LEET_CODE_API_URL + '/dailyQuestion')
+    .then(resp => {
+        selectedQuestion.value = {
+            title: resp.data.data.activeDailyCodingChallengeQuestion.question.title,
+            questionHTML: resp.data.data.activeDailyCodingChallengeQuestion.question.content,
+        }
+    })
+    .catch(err => toast.error(err))
+    .finally(() => isLoading.value = false);
 </script>
